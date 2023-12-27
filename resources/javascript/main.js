cf76fe0c960ebb7c442733bd12d73e5337c66f9d
fmpberger88 <fabian.berger@protonmail.ch>
2023-12-27 23:02:23 +0100

    impressum include website, delete fmpberger in footer

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,11 +1,42 @@
-let imageBox = document.querySelector('.image-box')
+document.addEventListener('DOMContentLoaded', function() {
+    function createBubble() {
+        const sectionIntro = document.querySelector('.intro');
+        const bubble = document.createElement('div');
+        bubble.classList.add('bubble');
 
-imageBox.addEventListener('touchstart', function () {
-    imageBox.style.transform = 'scale(1.5)';
-    imageBox.style.transform = 'transform 0.3s ease-in-out';
+        const size = Math.random() * 60 + 20; // Bubble size between 20px and 80px
+        bubble.style.width = `${size}px`;
+        bubble.style.height = `${size}px`;
+
+        bubble.style.backgroundColor = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.7)`;
+
+        bubble.style.left = `${Math.random() * 100}%`; // Random horizontal position
+
+        sectionIntro.appendChild(bubble);
+
+        // Remove bubble after it animates out
+        setTimeout(() => {
+            bubble.remove();
+        }, 5000); // Assuming animation takes 5 seconds
+    }
+
+    // Create a new bubble every 500 milliseconds
+    setInterval(createBubble, 500);
+
+    // You might want to add a limit on how many bubbles can exist at the same time
 });
 
-imageBox.addEventListener('touchend', function() {
-    imageBox.style.transform = 'scale(1)';
-    imageBox.style.transition = 'transform 0.3s ease-in-out';
+document.addEventListener('DOMContentLoaded', function() {
+    var cookiePopup = document.getElementById('cookiePopup');
+    var acceptCookies = document.getElementById('acceptCookies');
+
+    acceptCookies.onclick = function() {
+        cookiePopup.style.display = 'none';
+        localStorage.setItem('cookiesAccepted', 'true');
+    };
+
+    if (!localStorage.getItem('cookiesAccepted')) {
+        cookiePopup.style.display = 'block';
+    }
 });
+
